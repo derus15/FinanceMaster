97e5316a39fa0d63e250ac70e69a0ec59185c0dd
derus15 <9608212661i@gmail.com>
2023-09-05 14:04:57 +0400

    add function for sidebar

--- src/components/Charts/Chart.jsx
+++ src/components/Charts/Chart.jsx
@@ -12,55 +12,56 @@ import {
 } from 'chart.js';
 
 const Chart = () => {
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend,
+    );
 
-        ChartJS.register(
-            CategoryScale,
-            LinearScale,
-            PointElement,
-            LineElement,
-            Title,
-            Tooltip,
-            Legend,
-        );
-
-        const options = {
-            labels: [],
-            plugins: {
-                legend: {
-                    position: 'bottom',
-                    labels: {
-                        font: {
-                            size: 15,
-                            family: 'Montserrat, sans-serif',
-                        },
+    const options = {
+        labels: [1,2,3],
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    font: {
+                        size: 15,
+                        family: 'Montserrat, sans-serif',
                     },
                 },
             },
-        };
+        },
+    };
 
-        const data = {
-            datasets: [
-                {
-                    id: 1,
-                    label: '11',
-                    data: [5, 6, 7],
-                },
-                {
-                    id: 2,
-                    label: '112',
-                    data: [3, 2, 1],
-                },
-            ],
-        }
-
-
-        return (
-
-            <div style={{width: '100wh', height: '90vh'}}>
-                <Line options={options} datasetIdKey='id' data={data}/>
-            </div>
-        );
+    const data = {
+        datasets: [
+            {
+                id: 1,
+                backgroundColor: 'red',
+                borderColor: 'red',
+                label: '11',
+                data: [1,2],
+            },
+            {
+                id: 2,
+                backgroundColor: 'red',
+                borderColor: 'red',
+                label: '112',
+                data: [1,3],
+            },
+        ],
     }
-;
+
+    return (
+
+        <div style={{width: '90wh', height: '90vh'}}>
+            <Line options={options} data={data}/>
+        </div>
+    );
+};
 
 export default Chart;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Button, Container, Grid, Paper, TextField} from "@mui/material";
 
 const Sidebar = () => {
 
+    const [moneyInvest, setMoneyInvest] = useState(50);
 
+    const incrementMoney = () => {
+        if (moneyInvest >= 0) {
+            setMoneyInvest(prevState => prevState + 50);
+        }
+    }
 
+    const decrementMoney = () => {
+        if (moneyInvest >= 0) {
+            setMoneyInvest(prevState => prevState - 50);
+        }
+
+    }
+
+    const inputInvest = (e) => {
+        setMoneyInvest(Number(e.target.value))
+    }
 
     return (
         <Container maxWidth="sm">
@@ -16,17 +32,32 @@ const Sidebar = () => {
                 right: '0',
                 textAlign: 'center'
             }}>
-                <TextField label={'Investment'} size={'small'} margin="normal" sx={{bgcolor: 'white'}}/>
+                <TextField label={'Investment'} size={'small'} value={moneyInvest} margin="normal"
+                           sx={{bgcolor: 'white'}} onInput={inputInvest}/>
                 <Grid container justifyContent={'center'} gap={'5px'}>
-                    <Button variant="outlined" sx={{width: '39%'}}>+</Button>
-                    <Button variant="outlined" sx={{width: '39%'}}>-</Button>
+                    <Button variant="outlined" sx={{width: '39%'}} onClick={incrementMoney}>+</Button>
+                    <Button variant="outlined" sx={{width: '39%'}} onClick={decrementMoney}>-</Button>
                 </Grid>
                 <Grid container sx={{mt: '200px'}} direction={"column"} alignContent={'center'}>
-                    <Button variant="outlined" sx={{width: '224px', marginBottom: '10px', bgcolor: '#09AB19FF', color: 'white', height:'50px'}}>Up</Button>
+                    <Button variant="outlined" sx={{
+                        width: '224px',
+                        marginBottom: '10px',
+                        bgcolor: '#09AB19FF',
+                        color: 'white',
+                        height: '50px'
+                    }}>Up</Button>
                     <Paper sx={{width: '224px', height: '65px', fontSize: '23px', marginBottom: '10px'}}>Payout:<br/>120$</Paper>
-                    <Button variant="outlined" sx={{width: '224px', backgroundColor: 'red', color:'white', height:'50px' }}>Down</Button>
+                    <Button variant="outlined"
+                            sx={{width: '224px', backgroundColor: 'red', color: 'white', height: '50px'}}>Down</Button>
                 </Grid>
-                <div style={{position:'fixed', bottom:'0', fontSize:'15px', width:'280px', paddingBottom:'10px'}}>Все права защищены</div>
+                <div style={{
+                    position: 'fixed',
+                    bottom: '0',
+                    fontSize: '15px',
+                    width: '280px',
+                    paddingBottom: '10px'
+                }}>Все права защищены
+                </div>
             </Box>
         </Container>
 
